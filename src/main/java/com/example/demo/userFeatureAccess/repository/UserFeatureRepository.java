147f0a74e3110769077478abbd89c262ead12ee7
saurabh <saurabh.das@blazeclan.com>
2021-02-08 13:17:04 +0800

    Add Unit test case and verify the result

--- src/main/java/com/example/demo/userFeatureAccess/repository/UserFeatureRepository.java
+++ src/main/java/com/example/demo/userFeatureAccess/repository/UserFeatureRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.userFeatureAccess.repository;
 
 import com.example.demo.userFeatureAccess.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserFeatureRepository extends CrudRepository<User, Integer> {
 
     User findByEmailAndFeatureName(String email, String featureName);
